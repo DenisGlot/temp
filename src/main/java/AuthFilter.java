d7c20c73f346d9b43881ce4ef38c19f829837ad8
Admin <Admin@10.54.206.167>
2017-11-22 18:34:54 +0300

    with JdbcTemplate

--- src/main/java/AuthFilter.java
+++ src/main/java/AuthFilter.java
@@ -35,7 +35,7 @@ public class AuthFilter implements Filter {
  
 	@Override
 	public void destroy() {
-		System.out.println("****destroy AuthFilter");
+		System.out.println("***destroy AuthFilter");
 	}
 
 	@Override
