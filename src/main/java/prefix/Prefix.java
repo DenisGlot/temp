e08a2392eaf311518ad9c02fa0a4775945d6aae1
Denis <Denis@DESKTOP-CMUJ1S4>
2017-12-16 20:17:51 +0300

    Changed prefix

--- src/main/java/prefix/Prefix.java
+++ src/main/java/prefix/Prefix.java
@@ -3,7 +3,7 @@ package prefix;
 public class Prefix {
 
 	//Development state
-	public static final String prefix = "/calculator-0.0.1-SNAPSHOT";
+//	public static final String prefix = "/calculator-0.0.1-SNAPSHOT";
 	//Production state
-//	public static final String prefix = "";
+	public static final String prefix = "";
 }
